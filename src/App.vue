47c18314d558430f6194601b309a675ddce7c6cb
THINH NGUYEN TIEN <ntthinh@rd.local>
2022-10-06 17:24:16 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,44 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="grapes__selects">
+
+  </div>
+
+  <div class="grapes__drawboard">
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
 
 <style>
+
+body {
+  margin: 0px;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;
+
+  display: flex;
+}
+
+.grapes__selects {
+  width: 432px;
+  height: 100%;
+  background-color: red;
+
+}
+
+.grapes__drawboard {
+  width: calc(100% - 432px);
+  height: 100%;
+  background-color: yellow;
 }
 </style>
